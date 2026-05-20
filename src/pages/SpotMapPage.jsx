c6e8fb07c8ecbc677ed604d70dc02e87763dfa9b
Leandro <leandro.barchiesi@gmail.com>
2026-05-20 11:14:15 +0200

    leaflet vite fix

--- src/pages/SpotMapPage.jsx
+++ src/pages/SpotMapPage.jsx
@@ -1,5 +1,25 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
+
 import 'leaflet/dist/leaflet.css'
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+
+iconRetinaUrl:markerIcon2x,
+
+iconUrl:markerIcon,
+
+shadowUrl:markerShadow
+
+})
+
 
 export default function SpotMapPage(){
 
@@ -17,7 +37,9 @@ return(
 
 height:'80vh',
 
-width:'100%'
+width:'100%',
+
+background:'white'
 
 }}>
 
@@ -38,6 +60,8 @@ width:'100%'
 >
 
 <TileLayer
+
+attribution='OpenStreetMap'
 
 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
